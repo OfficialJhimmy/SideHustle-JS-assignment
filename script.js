88e51b9e8b5a490097d9b9688627a0e2fd3d444d
Erinle Jimi <72657872+OfficialJhimmy@users.noreply.github.com>
2020-11-01 09:32:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ let firstName = prompt("Please Enter Your Name");
 let hoursWorked = prompt(`How many hours did you work today ${firstName} ?`);
 
 
+
 function calcDaySalary (hoursWorked, minHours) {
     if (hoursWorked <= minHours) {
     let idealSalary = 1000 * hoursWorked;
     return (`${firstName} your Total salary is ${idealSalary} Naira.`);
     }
     if (hoursWorked > minHours) {
-    let idealSalary = (minHours * 1000) + ((hoursWorked - minHours) *200);
-    return (`${firstName} your basic salary was ${minHours * 1000} Naira, your Overtime Wages was ${(hoursWorked - minHours) *200} Naira. Hence Your Total salary is ${idealSalary} Naira`);
+    let idealSalary = (hoursWorked * 1000);
+    let overtimeWages = ((hoursWorked - minHours) * 200);
+    return (`${firstName} your basic salary was ${idealSalary} Naira, your Overtime Wages was ${overtimeWages} Naira. Hence Your Total salary is ${idealSalary + overtimeWages} Naira`);
     }
 }
 
